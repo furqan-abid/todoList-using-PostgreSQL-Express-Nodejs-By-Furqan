4057d7bff8f69ae91a07b8aa1f303808a3d417d2
furqan-abid <furqanabid10@gmail.com>
2023-11-24 15:29:05 +0500

    git ignore added

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool
 
+// const pool = new Pool({
+//     user:'postgres',
+//     password:"13252",
+//     host:'localhost',
+//     port:5432,
+//     database:'perntodo',
+// })
+
 const pool = new Pool({
-    user:'postgres',
-    password:"13252",
-    host:'localhost',
-    port:5432,
-    database:'perntodo',
-})
+    user: 'qbaqmmad', 
+    host: 'cornelius.db.elephantsql.com', 
+    database: 'qbaqmmad', 
+    password: 'fY5I9t1U3h6RrIr7gw9_ERlw4z5r1gP9', 
+    port: 5432, 
+    ssl: {
+      rejectUnauthorized: false, // For testing; use true in production with proper certificates
+    },
+  });
 
 module.exports = pool
